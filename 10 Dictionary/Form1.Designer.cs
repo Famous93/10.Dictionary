67c53fb22bb8a024c541e72fd70910bd27993aa3
Famous93 <73938606+Famous93@users.noreply.github.com>
2021-05-10 16:46:21 +0300

    Add project files.

---
+++ 10 Dictionary/Form1.Designer.cs
@@ -0,0 +1,235 @@
+﻿
+namespace _10_Dictionary
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.Enterkey = new System.Windows.Forms.TextBox();
+            this.keyValue = new System.Windows.Forms.TextBox();
+            this.Keyhaku = new System.Windows.Forms.TextBox();
+            this.Luo = new System.Windows.Forms.Button();
+            this.Lisää = new System.Windows.Forms.Button();
+            this.Haku = new System.Windows.Forms.Button();
+            this.Valuehaku = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ImageAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.label1.Location = new System.Drawing.Point(98, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(113, 22);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "LUODAAN Dictionary";
+            // 
+            // label2
+            // 
+            this.label2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ImageAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.label2.Location = new System.Drawing.Point(98, 141);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(140, 22);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "LISÄTÄÄN ELEMENTTEJÄ";
+            // 
+            // label3
+            // 
+            this.label3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ImageAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.label3.Location = new System.Drawing.Point(98, 292);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(140, 22);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "HAETAAN ELEMENTTEJÄ";
+            // 
+            // label4
+            // 
+            this.label4.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ImageAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.label4.Location = new System.Drawing.Point(12, 181);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(43, 22);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Key:";
+            // 
+            // label5
+            // 
+            this.label5.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ImageAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.label5.Location = new System.Drawing.Point(12, 224);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(43, 22);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Value:";
+            // 
+            // label6
+            // 
+            this.label6.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ImageAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.label6.Location = new System.Drawing.Point(12, 340);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(43, 22);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Key:";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // label7
+            // 
+            this.label7.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ImageAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.label7.Location = new System.Drawing.Point(12, 375);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(43, 22);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Value:";
+            // 
+            // Enterkey
+            // 
+            this.Enterkey.Location = new System.Drawing.Point(61, 178);
+            this.Enterkey.Name = "Enterkey";
+            this.Enterkey.Size = new System.Drawing.Size(150, 21);
+            this.Enterkey.TabIndex = 7;
+            // 
+            // keyValue
+            // 
+            this.keyValue.Location = new System.Drawing.Point(61, 221);
+            this.keyValue.Name = "keyValue";
+            this.keyValue.Size = new System.Drawing.Size(150, 21);
+            this.keyValue.TabIndex = 8;
+            // 
+            // Keyhaku
+            // 
+            this.Keyhaku.Location = new System.Drawing.Point(61, 337);
+            this.Keyhaku.Name = "Keyhaku";
+            this.Keyhaku.Size = new System.Drawing.Size(100, 21);
+            this.Keyhaku.TabIndex = 9;
+            // 
+            // Luo
+            // 
+            this.Luo.Location = new System.Drawing.Point(115, 83);
+            this.Luo.Name = "Luo";
+            this.Luo.Size = new System.Drawing.Size(75, 25);
+            this.Luo.TabIndex = 10;
+            this.Luo.Text = "Luo Dictionary";
+            this.Luo.UseVisualStyleBackColor = true;
+            this.Luo.Click += new System.EventHandler(this.Luo_Click);
+            // 
+            // Lisää
+            // 
+            this.Lisää.Location = new System.Drawing.Point(241, 218);
+            this.Lisää.Name = "Lisää";
+            this.Lisää.Size = new System.Drawing.Size(75, 25);
+            this.Lisää.TabIndex = 11;
+            this.Lisää.Text = "Lisää";
+            this.Lisää.UseVisualStyleBackColor = true;
+            this.Lisää.Click += new System.EventHandler(this.Lisää_Click);
+            // 
+            // Haku
+            // 
+            this.Haku.Location = new System.Drawing.Point(241, 337);
+            this.Haku.Name = "Haku";
+            this.Haku.Size = new System.Drawing.Size(75, 25);
+            this.Haku.TabIndex = 12;
+            this.Haku.Text = "Hae";
+            this.Haku.UseVisualStyleBackColor = true;
+            this.Haku.Click += new System.EventHandler(this.Haku_Click);
+            // 
+            // Valuehaku
+            // 
+            this.Valuehaku.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.Valuehaku.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Valuehaku.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.Valuehaku.Font = new System.Drawing.Font("Microsoft PhagsPa", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Valuehaku.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Valuehaku.Location = new System.Drawing.Point(62, 375);
+            this.Valuehaku.Name = "Valuehaku";
+            this.Valuehaku.Size = new System.Drawing.Size(57, 21);
+            this.Valuehaku.TabIndex = 13;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 14F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(354, 425);
+            this.Controls.Add(this.Valuehaku);
+            this.Controls.Add(this.Haku);
+            this.Controls.Add(this.Lisää);
+            this.Controls.Add(this.Luo);
+            this.Controls.Add(this.Keyhaku);
+            this.Controls.Add(this.keyValue);
+            this.Controls.Add(this.Enterkey);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Tai Le", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox Enterkey;
+        private System.Windows.Forms.TextBox keyValue;
+        private System.Windows.Forms.TextBox Keyhaku;
+        private System.Windows.Forms.Button Luo;
+        private System.Windows.Forms.Button Lisää;
+        private System.Windows.Forms.Button Haku;
+        private System.Windows.Forms.Label Valuehaku;
+    }
+}
+
---
+++ 10 Dictionary/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _10_Dictionary
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        IDictionary<string, string> luo;
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Luo_Click(object sender, EventArgs e)
+        {
+            luo = new Dictionary<string, string>();
+        }
+
+        private void Lisää_Click(object sender, EventArgs e)
+        {
+            luo.Add(Enterkey.Text, keyValue.Text);
+        }
+
+        private void Haku_Click(object sender, EventArgs e)
+        {
+            if (luo.ContainsKey(Keyhaku.Text))
+            {
+                Valuehaku.Text = luo[Keyhaku.Text];
+            }
+            else
+            {
+                Valuehaku.Text = "unknown";
+            }
+
+        }
+    }
+}
